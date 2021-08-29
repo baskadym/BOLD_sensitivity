d8c4bc5faa915901ecb577e84abe329564cf868c
Barbara Dymerska <b.dymerska@ucl.ac.uk>
2021-08-29 22:35:27 +0100

    Update TE_local_calc_share.m

--- local_TE_mapping_pack/TE_local_calc_share.m
+++ local_TE_mapping_pack/TE_local_calc_share.m
@@ -10,13 +10,13 @@
 %%%%%%%%%%% USER PARAMETERS %%%%%%%%%%%
 
 root_dir = '' ; % this is also where the local TE maps will be saved
-GEFM_name = 'fm_romeo_axial.nii' ; % unwrapped gradient echo fieldmap in Hz
-
+GEFM_name = 'wB0.nii' ; % unwrapped gradient echo fieldmap in Hz
+PE_dir_dim = 1 ; % which coordinate (1st, 2nd or 3rd?) from the reference data will be the PE-direction in the EPI target data?
 
 % EPI parameters
 Tesp = 0.0012; % the effective echo spacing in EPI in sec
 iPAT = 8 ; % acceleration factor in phase encoding direction
-PE_dir = 1 ; % 1 for Posterior-Anterior and -1 for Anterior-Posterior
+PE_dir = -1 ; % 1 for Posterior-Anterior and -1 for Anterior-Posterior
 pF = 1 ; % partial fourier, set to 1 if first part of k-space is not acquired, set to true value if last part of k-space is not acquired
 TE = 0.0177 ; % the effective echo time in seconds set as sequence parameter
 my = 200 ; % matrix size in phase encodng direction
@@ -28,18 +28,31 @@ flag_voxels_out = 'no' ; % select 'yes' or 'no', if 'yes' flags voxels with sign
 
 disp('loading GE field map') ;
 GEFM_file = fullfile(root_dir,GEFM_name) ;
-GEFM_nii = load_nii(GEFM_file) ;
-[Gy,~,~] = gradient(PE_dir*GEFM_nii.img) ; % in gradient function x is swaped with y, i.e. the gradient in the PE-direction is in x-direction
+GEFM_V = spm_vol(GEFM_file) ;
+GEFM = GEFM_V.private.dat(:,:,:) ;
+GEFM(isnan(GEFM)) = 0 ;
+
+if PE_dir_dim == 1
+    [G_PE,~,~] = gradient(PE_dir*GEFM) ; % in gradient function x is swaped with y, i.e. the gradient in the PE-direction is in x-direction
+elseif PE_dir_dim == 2
+    [~,G_PE,~] = gradient(PE_dir*GEFM) ; % in gradient function x is swaped with y, i.e. the gradient in the PE-direction is in x-direction
+elseif PE_dir_dim == 3
+    [~,~,G_PE] = gradient(PE_dir*GEFM) ; % in gradient function x is swaped with y, i.e. the gradient in the PE-direction is in x-direction
+else
+    error('PE_dir_dim must be 1 or 2 or 3')
+end
 
 
-Gy_nii = make_nii(Gy, GEFM_nii.hdr.dime.pixdim(2:4)) ;
-Gy_file = fullfile(root_dir, 'GradY_GEFM.nii') ;
-save_nii(Gy_nii, Gy_file) ;
+GEFM_V.descrip = 'Gradient in PE-direction' ;
+if PE_dir == 1
+    GEFM_V.fname = fullfile(root_dir, 'GradY_GEFM_PA.nii') ;
+elseif PE_dir == -1
+    GEFM_V.fname = fullfile(root_dir, 'GradY_GEFM_AP.nii') ;
+end
+G_PE(isnan(G_PE)) = 0 ;
+spm_write_vol(GEFM_V, G_PE) ;
 
 output_dir = root_dir ;
-GEFM_nii.img(isnan(GEFM_nii.img))=0 ;
-
-Gy_bw_nii = Gy_nii ;
 
 
 disp('calculating the effective echo spacing') ;
@@ -49,9 +62,9 @@ disp('calculating maximum TE allowed, above which type II loses occur (signal go
 TE_max = my*pF*Tesp ;
 
 disp('calculating local effective TE') ;
-TE_mat = repmat(TE, size(Gy_bw_nii.img)) ;
-my_mat = repmat(1/my, size(Gy_bw_nii.img)) ;
-TEloc = TE_mat + (-Gy_bw_nii.img*TE*Tesp./(my_mat+Gy_bw_nii.img.*Tesp)) ;
+TE_mat = repmat(TE, size(G_PE)) ;
+my_mat = repmat(1/my, size(G_PE)) ;
+TEloc = TE_mat + (-G_PE*TE*Tesp./(my_mat+G_PE.*Tesp)) ;
 
 if strcmp(flag_voxels_out,'yes')
     disp('flagging of the values with type II signal loss (flag = -1000)') ;
@@ -62,15 +75,24 @@ if strcmp(flag_voxels_out,'yes')
     TE_flags(TEloc==-1000) = 1 ;
     
     disp('saving flag mask') ;
-    TE_flags_nii = make_nii(TE_flags, GEFM_nii.hdr.dime.pixdim(2:4)) ;
-    TE_flags_file = fullfile(root_dir, 'TE_flags.nii') ;
-    save_nii(TE_flags_nii, TE_flags_file) ;
+    
+    GEFM_V.descrip = 'Mask for voxels for which signal will fall outside the k-space acquisition window' ;
+    if PE_dir == 1
+        GEFM_V.fname = fullfile(root_dir, 'TE_flags_PA.nii') ;
+    elseif PE_dir == -1
+        GEFM_V.fname = fullfile(root_dir, 'TE_flags_AP.nii') ;
+    end
+    spm_write_vol(GEFM_V, TE_flags) ;
 end
 
 disp('saving local TE') ;
-TEloc_nii = make_nii(TEloc, GEFM_nii.hdr.dime.pixdim(2:4)) ;
-TEloc_file = fullfile(root_dir, 'TE_local.nii') ;
-save_nii(TEloc_nii, TEloc_file) ;
+GEFM_V.descrip = 'Map of local effective echo time' ;
+if PE_dir == 1
+    GEFM_V.fname = fullfile(root_dir, 'TE_local_PA.nii') ;
+elseif PE_dir == -1
+    GEFM_V.fname = fullfile(root_dir, 'TE_local_AP.nii') ;
+end
+spm_write_vol(GEFM_V, TEloc) ;
 
 
 
